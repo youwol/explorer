70cecbd8601018802cca8bf7c0ddcc3242fb424d
greinisch <greinisch@youwol.com>
2021-04-16 18:42:46 +0200

    :arrow_up:

--- src/app/main.ts
+++ src/app/main.ts
@@ -12,8 +12,8 @@ await cdn.fetchStyleSheets([
 await cdn.fetchBundles(
       { d3:'5.15.0',
         lodash:'4.17.15',
-        '@youwol/flux-view':'0.0.5',
-        '@youwol/flux-core': '0.0.6',
+        '@youwol/flux-view':'0.0.6',
+        '@youwol/flux-core': '0.0.7',
         "@youwol/fv-group":"0.0.3",
         "@youwol/fv-input":"0.0.2",
         "@youwol/fv-button":"0.0.3",

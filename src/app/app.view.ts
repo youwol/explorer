1c0b959e2c0e5449aced7498d91f390b6c34da4c
greinisch <greinisch@youwol.com>
2021-12-29 16:46:09 +0100

    :bookmark: standalone app #0.0.1

--- src/app/app.view.ts
+++ src/app/app.view.ts
@@ -1,24 +1,67 @@
-import { VirtualDOM } from '@youwol/flux-view'
+import { child$, VirtualDOM } from '@youwol/flux-view'
 import { BehaviorSubject } from 'rxjs'
-import { PlatformBannerView, PlatformState, SideBarView, MainPanelView } from '@youwol/platform-essentials'
+import {
+    SideBarView, YouwolBannerState,
+    YouwolBannerView, defaultUserMenu, defaultYouWolMenu, HeaderPathView, DisplayMode,
+    FolderContentView, ExplorerState
+} from '@youwol/platform-essentials'
 
+
+/**
+ * Top banner of the application
+ */
+export class TopBannerView extends YouwolBannerView {
+
+    constructor(params: { state: AppState }) {
+        super({
+            state: params.state.topBannerState,
+            customActionsView: new HeaderPathView({ state: params.state, class: 'mx-auto w-100 fv-bg-background d-flex align-items-center' } as any),
+            userMenuView: defaultUserMenu(params.state.topBannerState),
+            youwolMenuView: defaultYouWolMenu(params.state.topBannerState)
+        })
+    }
+}
+
+export class AppState extends ExplorerState {
+
+    public readonly displayMode$ = new BehaviorSubject<DisplayMode>('details')
+    public readonly topBannerState = new YouwolBannerState()
+
+    constructor() {
+        super()
+    }
+}
 
 export class AppView implements VirtualDOM {
 
     class = 'h-100 w-100 d-flex flex-column fv-text-primary'
-    state = new PlatformState()
+    state = new AppState()
     children: VirtualDOM[]
 
     constructor() {
 
         this.children = [
-            new PlatformBannerView({ state: this.state }),
+            new TopBannerView({ state: this.state }),
             {
                 class: 'flex-grow-1 w-100 d-flex',
                 style: { minHeight: '0px' },
                 children: [
                     new SideBarView(this.state, new BehaviorSubject(false)),
-                    new MainPanelView({ state: this.state })
+                    {
+                        class: 'w-100 h-100 d-flex',
+                        children: [
+                            child$(
+                                this.state.currentFolder$,
+                                ({ folder }) => {
+                                    return new FolderContentView({
+                                        state: this.state,
+                                        folderId: folder.id,
+                                        groupId: folder.groupId
+                                    })
+                                }
+                            )
+                        ]
+                    }
                 ]
             }
         ]

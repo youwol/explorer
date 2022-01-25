490656273379758635cba21e0370f8f4ee08dedb
greinisch <greinisch@youwol.com>
2022-01-25 11:20:08 +0100

    :wrench: update yw_pipeline

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,8 +1,13 @@
-from youwol.configuration import Pipeline
-from youwol.configuration import UserConfiguration
-
-def pipeline(configuration: UserConfiguration) -> Pipeline:
-
-    return configuration.get_custom_pipeline("typescript-webpack-npm")
+import youwol.pipelines.pipeline_typescript_weback_npm
+from youwol.environment.forward_declaration import YouwolEnvironment
+from youwol.environment.models import IPipelineFactory
+from youwol_utils.context import Context
 
 
+class PipelineFactory(IPipelineFactory):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def get(self, _env: YouwolEnvironment, _ctx: Context):
+        return youwol.pipelines.pipeline_typescript_weback_npm.pipeline()

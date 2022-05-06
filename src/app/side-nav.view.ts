bdd209e8036fe9344faa3e9b9df1edb14aba4846
greinisch <greinisch@youwol.com>
2022-05-06 07:51:11 +0200

    ♻️ factorize Groups & User tabs + expose favorite folders

--- src/app/side-nav.view.ts
+++ src/app/side-nav.view.ts
@@ -1,11 +1,18 @@
 import { DockableTabs } from '@youwol/fv-tabs'
-import { child$, VirtualDOM, attr$ } from '@youwol/flux-view'
+import { child$, VirtualDOM } from '@youwol/flux-view'
 import { LeftNavTopic } from './app.view'
 import { Explorer } from '@youwol/platform-essentials'
 import { ImmutableTree } from '@youwol/fv-tree'
 import { ContextMenu } from '@youwol/fv-context-menu'
 import { ContextMenuState } from './context-menu.view'
-import { map } from 'rxjs/operators'
+import { filter, map, mergeMap, take } from 'rxjs/operators'
+import { AssetsGateway } from '@youwol/http-clients'
+import { BehaviorSubject } from 'rxjs'
+import { Select } from '@youwol/fv-input'
+import {
+    AnyItemNode,
+    FolderNode,
+} from '@youwol/platform-essentials/src/lib/explorer/nodes'
 
 export class LeftNavTab extends DockableTabs.Tab {
     protected constructor(params: {
@@ -30,9 +37,12 @@ export class UserDriveTab extends LeftNavTab {
                 return child$(
                     params.state.defaultUserDrive$,
                     (defaultUserDrive) => {
-                        return new TreeViewDrive({
+                        return new GroupView({
                             explorerState: params.state,
-                            groupId: defaultUserDrive.groupId,
+                            treeGroup:
+                                params.state.groupsTree[
+                                    defaultUserDrive.groupId
+                                ],
                         })
                     },
                 )
@@ -42,8 +52,64 @@ export class UserDriveTab extends LeftNavTab {
     }
 }
 
+export class GroupsTab extends LeftNavTab {
+    constructor(params: { state: Explorer.ExplorerState }) {
+        super({
+            topic: 'Groups',
+            title: 'Groups',
+            icon: 'fas fa-users',
+            state: params.state,
+            content: () => {
+                return child$(
+                    params.state.userInfo$,
+                    (userInfo: AssetsGateway.UserInfoResponse) => {
+                        return new GroupsTabView({
+                            explorerState: params.state,
+                            userInfo,
+                        })
+                    },
+                )
+            },
+        })
+        Object.assign(this, params)
+    }
+}
+
+export class GroupView implements VirtualDOM {
+    public readonly children: VirtualDOM[]
+    public readonly explorerState: Explorer.ExplorerState
+    public readonly treeGroup: Explorer.TreeGroup
+
+    constructor(params: {
+        explorerState: Explorer.ExplorerState
+        treeGroup: Explorer.TreeGroup
+    }) {
+        Object.assign(this, params)
+        this.children = [
+            child$(this.explorerState.favoriteFolders$, (favoritesFolder) => {
+                return new FavoritesTabView({
+                    explorerState: this.explorerState,
+                    favoritesFolder: favoritesFolder.filter(
+                        (f) => f.groupId == this.treeGroup.groupId,
+                    ),
+                })
+            }),
+            new TreeViewDrive({
+                explorerState: this.explorerState,
+                treeGroup: this.treeGroup,
+            }),
+        ]
+    }
+}
 export class TreeViewDrive extends ImmutableTree.View<Explorer.BrowserNode> {
     public readonly explorerState: Explorer.ExplorerState
+    public readonly treeGroup: Explorer.TreeGroup
+    static baseWrapperHeaderClass =
+        'align-items-baseline fv-tree-header fv-hover-bg-background-alt rounded'
+    static wrapperHeaderClassFct = (node) =>
+        `${TreeViewDrive.baseWrapperHeaderClass} ${
+            node instanceof Explorer.GroupNode ? 'd-none' : 'd-flex '
+        }`
 
     public readonly connectedCallback = (elem) => {
         return new ContextMenu.View({
@@ -61,33 +127,144 @@ export class TreeViewDrive extends ImmutableTree.View<Explorer.BrowserNode> {
 
     constructor(params: {
         explorerState: Explorer.ExplorerState
-        groupId: string
+        treeGroup: Explorer.TreeGroup
     }) {
         super({
-            state: params.explorerState.groupsTree[params.groupId], //new TreeViewState(params),
+            state: params.treeGroup, //params.explorerState.groupsTree[params.groupId], //new TreeViewState(params),
             headerView,
             options: {
                 classes: {
-                    header: 'd-flex align-items-baseline fv-tree-header fv-hover-bg-background-alt rounded',
+                    header: TreeViewDrive.wrapperHeaderClassFct,
+                    headerSelected: 'd-flex fv-text-focus',
                 },
             },
             dropAreaView: () => ({ class: 'w-100 my-1' }),
         })
         Object.assign(this, params)
-        this.explorerState.openFolder$.subscribe((d) => {
-            this.state.selectedNode$.next(d.folder)
-            const expanded = this.state.expandedNodes$.getValue()
-            if (!expanded.includes(d.folder.id)) {
-                this.state.selectNodeAndExpand(d.folder)
-            }
+        this.treeGroup.expandedNodes$.next([
+            this.treeGroup.groupId,
+            this.treeGroup.defaultDriveId,
+            this.treeGroup.homeFolderId,
+        ])
+        this.explorerState.openFolder$
+            .pipe(
+                take(1),
+                filter(({ tree }) => tree == this.treeGroup),
+            )
+            .subscribe((d) => {
+                this.treeGroup.selectNodeAndExpand(d.folder)
+            })
+    }
+}
+
+class GroupSelectItemData extends Select.ItemData {
+    constructor(public readonly group: AssetsGateway.GroupResponse) {
+        super(group.path, group.path.split('/').slice(-1)[0])
+    }
+}
+
+export class GroupsTabView implements VirtualDOM {
+    public readonly class = 'w-100'
+
+    public readonly children: VirtualDOM[]
+    public readonly explorerState: Explorer.ExplorerState
+    public readonly userInfo: AssetsGateway.UserInfoResponse
+    public readonly group$: BehaviorSubject<AssetsGateway.GroupResponse>
+    constructor(params: {
+        explorerState: Explorer.ExplorerState
+        userInfo: AssetsGateway.UserInfoResponse
+    }) {
+        Object.assign(this, params)
+        let sortGroup = (a, b) => (a.path.length < b.path.length ? -1 : 1)
+
+        const displayedGroups = this.userInfo.groups
+            .filter((g) => g.path != 'private')
+            .sort(sortGroup)
+
+        this.group$ = new BehaviorSubject<AssetsGateway.GroupResponse>(
+            displayedGroups[0],
+        )
+        const itemsData = displayedGroups.map((g) => new GroupSelectItemData(g))
+
+        const selectState = new Select.State(itemsData, itemsData[0].id)
+        this.children = [
+            new Select.View({
+                state: selectState,
+                class: 'w-100',
+            } as any),
+            child$(
+                selectState.selection$.pipe(
+                    mergeMap((item: GroupSelectItemData) =>
+                        this.explorerState.selectGroup$(item.group.id),
+                    ),
+                ),
+                (treeGroup) => {
+                    return new GroupView({
+                        explorerState: this.explorerState,
+                        treeGroup,
+                    })
+                },
+            ),
+        ]
+    }
+}
+
+export class FavoriteItemView implements VirtualDOM {
+    public readonly class =
+        'rounded fv-pointer px-1 m-1 fv-bg-background-alt fv-hover-xx-lighter'
+    public readonly children: VirtualDOM[]
+    public readonly explorerState: Explorer.ExplorerState
+    public readonly favoriteFolder: Explorer.FavoriteFolder
+
+    constructor(params: {
+        explorerState: Explorer.ExplorerState
+        favoriteFolder: Explorer.FavoriteFolder
+    }) {
+        Object.assign(this, params)
+
+        this.children = [
+            {
+                class: 'd-flex align-items-center',
+                children: [
+                    {
+                        class: 'fas fa-map-pin mr-2',
+                    },
+                    {
+                        innerText: this.favoriteFolder.name,
+                    },
+                ],
+                onclick: () => {
+                    this.explorerState.navigateTo(this.favoriteFolder.folderId)
+                },
+            },
+        ]
+    }
+}
+
+export class FavoritesTabView implements VirtualDOM {
+    public readonly class = 'w-100 d-flex flex-wrap overflow-auto'
+    public readonly style = {
+        maxHeight: '25%',
+    }
+    public readonly children
+    public readonly explorerState: Explorer.ExplorerState
+    public readonly favoritesFolder: Explorer.FavoriteFolder[]
+
+    constructor(params: {
+        explorerState: Explorer.ExplorerState
+        favoritesFolder: Explorer.FavoriteFolder[]
+    }) {
+        Object.assign(this, params)
+        this.children = this.favoritesFolder.map((folder) => {
+            return new FavoriteItemView({
+                explorerState: this.explorerState,
+                favoriteFolder: folder,
+            })
         })
     }
 }
 
-export function headerView(
-    state: Explorer.TreeGroup,
-    item: Explorer.BrowserNode,
-) {
+function headerView(state: Explorer.TreeGroup, item: Explorer.BrowserNode) {
     if (
         item instanceof Explorer.ItemNode ||
         item instanceof Explorer.FutureItemNode ||

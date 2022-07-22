ae18692ab245703349d8979ea8c57e3ebb39c2f0
greinisch <greinisch@youwol.com>
2022-07-22 10:27:03 +0200

    🐛 main.ts => use new cdn-client API to install dependencies + fix majors specification

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,36 +6,29 @@ require('./style.css')
 const loadingScreen = new LoadingScreenView()
 loadingScreen.render()
 
-await install(
-    {
-        modules: [
-            'lodash',
-            'rxjs',
-            'marked',
-            '@youwol/flux-view',
-            '@youwol/fv-group',
-            '@youwol/fv-button',
-            '@youwol/fv-tree',
-            '@youwol/fv-tabs',
-            '@youwol/fv-input',
-            '@youwol/fv-context-menu',
-            '@youwol/os-core',
-            '@youwol/os-explorer',
-            '@youwol/os-top-banner',
-            '@youwol/os-asset',
-        ],
-        css: [
-            'bootstrap#4.4.1~bootstrap.min.css',
-            'fontawesome#5.12.1~css/all.min.css',
-            '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
-        ],
+await install({
+    modules: [
+        'marked#3.x',
+        '@youwol/fv-group#0.x',
+        '@youwol/fv-button#0.x',
+        '@youwol/fv-tree#0.x',
+        '@youwol/fv-tabs#0.x',
+        '@youwol/fv-input#0.x',
+        '@youwol/fv-context-menu#0.x',
+        '@youwol/os-core#0.x',
+        '@youwol/os-explorer#0.x',
+        '@youwol/os-top-banner#0.x',
+        '@youwol/os-asset#0.x',
+    ],
+    css: [
+        'bootstrap#4.4.1~bootstrap.min.css',
+        'fontawesome#5.12.1~css/all.min.css',
+        '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
+    ],
+    onEvent: (ev) => {
+        loadingScreen.next(ev)
     },
-    {
-        onEvent: (ev) => {
-            loadingScreen.next(ev)
-        },
-    },
-)
+})
 
 Client['initialLoadingScreen'] = loadingScreen
 

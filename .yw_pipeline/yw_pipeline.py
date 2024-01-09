06aa175f85176a5bc5741a06f95cd1b0dede3d2e
jehadmelad <jd.melad@gmail.com>
2024-01-09 18:10:45 +0100

    ✨  yw_pipeline.py => change the importation of `IPipelineFactory`

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
 from youwol.app.environment import YouwolEnvironment
-from youwol.app.environment.models import IPipelineFactory
-from youwol.app.environment.models_project import BrowserApp, Execution, Link, BrowserAppGraphics
+from youwol.app.environment.models_project import BrowserApp, Execution, Link, BrowserAppGraphics, IPipelineFactory
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, PublishConfig
 from youwol.utils import parse_json, encode_id
 from youwol.utils.context import Context
